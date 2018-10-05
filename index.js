a19c638d21ef37e9140ec6fbab90d181224787bd
ayalon GmbH <info@ayalon.ch>
2018-10-05 15:13:49 +0200

    Create a unique id per filename

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ if (!sourcePath || !fs.existsSync(sourcePath)) {
   process.exit(1);
 }
 
+if (!fs.existsSync(destinationPath)){
+  fs.writeFile(destinationPath, '', function (err) {
+    if (err) throw err;
+    console.log('Empty file created!');
+  });
+}
+
+
 if (!destinationPath || !fs.existsSync(destinationPath)) {
   console.error("no destination path given!");
   process.exit(1);
@@ -41,8 +49,10 @@ for (let i = 0; i < filePathList.length; i++) {
   const content = fs.readFileSync(filePath, {
     encoding: "utf-8"
   });
+
+  const fileNameHash = hashFnv32a(path.basename(filePath));
   const query = compress(content);
-  queryMap[query] = uuid();
+  queryMap[query] = fileNameHash;
 }
 
 let writePath = destinationPath;
@@ -52,3 +62,31 @@ if (fs.lstatSync(writePath).isDirectory()) {
 
 const writeContent = JSON.stringify(queryMap);
 fs.writeFileSync(writePath, writeContent);
+
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} str the input value
+ * @param {boolean} [asString=false] set to true to return the hash value as
+ *     8-digit hex string instead of an integer
+ * @param {integer} [seed] optionally pass the hash of the previous chunk
+ * @returns {integer | string}
+ */
+function hashFnv32a(str, asString, seed) {
+  /*jshint bitwise:false */
+  var i, l,
+    hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+  for (i = 0, l = str.length; i < l; i++) {
+    hval ^= str.charCodeAt(i);
+    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  }
+  if( asString ){
+    // Convert to 8 digit hex string
+    return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+  }
+  return hval >>> 0;
+}
+
